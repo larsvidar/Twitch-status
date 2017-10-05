955ae6d38fcf9d750b9961514af1267842b3a6ca
Lars Vidar <larsvidar@larsvidar.no>
2017-10-05 06:05:56 +0200

    - Made list of Twitch-users. - Showing profile image. - Collecting user bio (but hiding it for small viewports)

--- script/twitchstatus.js
+++ script/twitchstatus.js
@@ -1,15 +1,42 @@
 console.log("Twitchstatus.js loaded!");
 
-var apiUserURL = "https://wind-bow.gomix.me/twitch-api/users/larsvidar?callback=?"
-var apiChannelURL = "https://wind-bow.gomix.me/twitch-api/channels/anniefuchsia?callback=?"
-var apiStreamURL = "https://wind-bow.gomix.me/twitch-api/streams/anniefuchsia?callback=?"
+const twitchUsers = ["larsvidar", "freecodecamp", "anniefuchsia", "esl_sc2", "esl_csgo", "kig666", "emzia", "voyboy", "strippin", "robotcaleb"];
+const defaultImage = "img/twitch-logo.png";
+let content = "";
 
-
-function getUser() {
-  $.getJSON(apiStreamURL, function(data) {
-    console.log(data);
-  });
-
+function makeURL(type, user) {
+  // type = "users", "channels" or "streams"
+  return "https://wind-bow.gomix.me/twitch-api/" + type + "/" + user + "?callback=?";
 }
 
-getUser();
+
+function getUser(type, user, value) {
+  $.getJSON(makeURL(type, user), function(data) {
+    console.log(data);
+    var result={};
+    value.forEach(function(item) {
+      result[item] = data[item];
+    });
+    showUser(result);
+  });
+}
+
+function showUser(values) {
+  content += "<li class='user'>";
+  content += "<h3 class='name'>" + values.display_name + "</h3>";
+  content += "<p class='bio'>" + values.bio + "</p>"
+  if (values.logo != null) {
+    content += "<img class='logo mr-0' src='" + values.logo + "'>";
+  } else {
+    content += "<img class='logo mr-0' src='" + defaultImage + "'>";
+  }
+  content += "</li>"
+  $("#user-list").html(content);
+}
+
+
+twitchUsers.forEach(function(userName) {
+  getUser("users", userName, ["display_name", "bio", "logo"]);
+});
+
+getUser("channels", "esl_csgo", ["display_name"]);
