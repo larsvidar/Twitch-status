1f28563d653513338d659f32c1e075f5f3af01f7
Lars Vidar <larsvidar@larsvidar.no>
2017-10-08 13:03:00 +0200

    - Added filter for showing online/offline channels - Working, but has to solve a problem with the requests (to many are being sent.)

--- script/twitchstatus.js
+++ script/twitchstatus.js
@@ -10,33 +10,73 @@ function makeURL(type, user) {
 }
 
 
-function getUser(type, user, value) {
-  $.getJSON(makeURL(type, user), function(data) {
-    console.log(data);
+function getUser(user, values) {
+  $.getJSON(makeURL("users", user), function(data) {
     var result={};
-    value.forEach(function(item) {
+    values.forEach(function(item) {
       result[item] = data[item];
     });
-    showUser(result);
+    getChannel(user, ["status"], result);
   });
 }
 
-function showUser(values) {
+function getChannel(user, values, result) {
+  $.getJSON(makeURL("channels", user), function(data) {
+    values.forEach(function(item) {
+      result[item] = data[item];
+    });
+    getStream(user, ["stream"], result);
+  });
+}
+
+function getStream(user, values, results) {
+  $.getJSON(makeURL("streams", user), function(data) {
+    values.forEach(function(item) {
+      results[item] = data[item];
+    });
+    showUser(results);
+  });
+}
+
+
+
+function showUser(results) {
+  console.log(results);
   content += "<li class='user'>";
-  content += "<h3 class='name'>" + values.display_name + "</h3>";
-  content += "<p class='bio'>" + values.bio + "</p>"
-  if (values.logo != null) {
-    content += "<img class='logo mr-0' src='" + values.logo + "'>";
+  content += "<h3 class='name'>" + results.display_name + "</h3>";
+  content += "<p class='bio'>" + results.bio + "</p>"
+  if (results.logo != null) {
+    content += "<img class='logo mr-0' src='" + results.logo + "'>";
   } else {
     content += "<img class='logo mr-0' src='" + defaultImage + "'>";
+  }
+  if (results.stream != null) {
+    content += "<div class='status tag green'></div>";
+  } else {
+    content += "<div class='status tag red'></div>";
   }
   content += "</li>"
   $("#user-list").html(content);
 }
 
+$("#user-list").html("<h2>Loading data from Twitch. Please wait!</h2>");
 
 twitchUsers.forEach(function(userName) {
-  getUser("users", userName, ["display_name", "bio", "logo"]);
+  getUser(userName, ["display_name", "bio", "logo"]);
 });
 
-getUser("channels", "esl_csgo", ["display_name"]);
+$(".map").on("click", function() {
+  if ($(this).hasClass("green")) {
+    $(".status").closest(".user").css("display", "block");
+    $(".red").closest(".user").css("display", "none");
+  } else if ($(this).hasClass("red")) {
+    $(".status").closest(".user").css("display", "block");
+    $(".green").closest(".user").css("display", "none");
+  } else {
+    $(".status").closest(".user").css("display", "block");
+  }
+});
+
+// $.getJSON("https://api.twitch.tv/kraken/users?login=dallas,dallasnchains", function(data) {
+//   console.log(data);
+// });
